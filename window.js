52d62d120aa88340e0e49d8a00cd315dc0a8f795
dachxd <106717421+D3ch@users.noreply.github.com>
2023-07-03 02:16:30 +0000

    window html

--- window.js
+++ window.js
@@ -1,55 +1,26 @@
-        // Big update soon?????
-        window.addEventListener('DOMContentLoaded', (event) => {
-            const windows = document.querySelectorAll('.window');
-            let selectedWindow = null;
-            let offsetX = 0;
-            let offsetY = 0;
+var windowElement = document.getElementById('window');
+    var iframeElement = document.getElementById('iframe');
 
-            windows.forEach((window) => {
-                window.addEventListener('mousedown', handleDragStart);
-                window.addEventListener('mouseup', handleDragEnd);
-                window.addEventListener('mousemove', handleDrag);
-            });
+    function loadPath(path) {
+      iframeElement.src = path;
+      windowElement.classList.add("active");
+    }
 
-            function handleDragStart(event) {
-                selectedWindow = event.target;
-                offsetX = event.offsetX;
-                offsetY = event.offsetY;
-                selectedWindow.style.zIndex = '1';
-                selectedWindow.style.opacity = '0.5';
-                selectedWindow.style.cursor = 'grabbing';
-            }
+    function dragWindow(event) {
+      var offsetX = event.clientX - windowElement.offsetLeft;
+      var offsetY = event.clientY - windowElement.offsetTop;
 
-            function handleDragEnd(event) {
-                selectedWindow.style.zIndex = '0';
-                selectedWindow.style.opacity = '1';
-                selectedWindow.style.cursor = 'move';
-                selectedWindow = null;
-            }
+      document.onmousemove = function(event) {
+        windowElement.style.left = (event.clientX - offsetX) + "px";
+        windowElement.style.top = (event.clientY - offsetY) + "px";
+      };
 
-            function handleDrag(event) {
-                if (selectedWindow) {
-                    const newLeft = event.clientX - offsetX;
-                    const newTop = event.clientY - offsetY;
+      document.onmouseup = function() {
+        document.onmousemove = null;
+      };
+    }
 
-                    selectedWindow.style.left = `${newLeft}px`;
-                    selectedWindow.style.top = `${newTop}px`;
-                }
-            }
-        });
-
-        // Toggle functionality
-        function toggleWindow(windowId) {
-            const window = document.getElementById(windowId);
-            if (window.style.display === 'none') {
-                window.style.display = 'block';
-            } else {
-                window.style.display = 'none';
-            }
-        }
-
-        // Close button functionality
-        function closeWindow(windowId) {
-            const window = document.getElementById(windowId);
-            window.style.display = 'none';
-        }
+    function closeWindow() {
+      windowElement.classList.remove("active");
+      iframeElement.src = '';
+    }
