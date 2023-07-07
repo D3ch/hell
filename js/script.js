f9e4068fec96595c6b65a930ee4ec190f815982f
dachxd <106717421+D3ch@users.noreply.github.com>
2023-07-07 22:19:17 +0000

    settings maybe pls work

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Function to change the theme
+function changeTheme(theme) {
+    document.getElementById('themeLink').href = `${theme}-theme.css`;
+    localStorage.setItem('currentTheme', theme);
+  }
+  
+  // Function to get the currently selected theme from localStorage
+  function getCurrentTheme() {
+    return localStorage.getItem('currentTheme');
+  }
+  
+  // Set the theme on page load
+  window.addEventListener('DOMContentLoaded', function() {
+    const savedTheme = getCurrentTheme();
+    if (savedTheme) {
+      changeTheme(savedTheme);
+    }
+  });
+  
+  // Event listener for the theme buttons
+  document.getElementById('darkThemeBtn').addEventListener('click', function() {
+    changeTheme('dark');
+  });
+  
+  document.getElementById('lightThemeBtn').addEventListener('click', function() {
+    changeTheme('light');
+  });
+  
+  document.getElementById('lightThemeBtn').addEventListener('click', function() {
+    changeTheme('default');
+  });
+  
+  // Clear the saved theme from localStorage
+  document.getElementById('clearThemeBtn').addEventListener('click', function() {
+    localStorage.removeItem('currentTheme');
+  });
