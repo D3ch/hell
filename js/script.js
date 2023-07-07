f7321b72e84b6de38d05fc77558a411fd9610e9f
dachxd <106717421+D3ch@users.noreply.github.com>
2023-07-07 22:35:08 +0000

    "oh its def gonna work"

--- js/script.js
+++ js/script.js
@@ -19,15 +19,15 @@ function changeTheme(theme) {
   
   // Event listener for the theme buttons
   document.getElementById('darkThemeBtn').addEventListener('click', function() {
-    changeTheme('dark');
+    changeTheme('/dark');
   });
   
   document.getElementById('lightThemeBtn').addEventListener('click', function() {
-    changeTheme('light');
+    changeTheme('/light');
   });
   
   document.getElementById('lightThemeBtn').addEventListener('click', function() {
-    changeTheme('default');
+    changeTheme('/default');
   });
   
   // Clear the saved theme from localStorage
