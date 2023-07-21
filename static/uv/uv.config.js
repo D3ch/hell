06972cd861f09c780a77c4463b57bb27809b25c3
dachxd <106717421+D3ch@users.noreply.github.com>
2023-07-20 19:29:01 -0700

    gtg

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
+    prefix: '/static/',
     bare: 'https://99e74781-413a-4718-8a5a-ed4f0556b3cb.id.repl.co/bare/',
     /*https://baresw.starttiw.org */
     encodeUrl: Ultraviolet.codec.xor.encode,
