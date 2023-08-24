21a9a6cb57293d9c837efccffd61f97eb15d6ef6
dachxd <106717421+D3ch@users.noreply.github.com>
2023-08-23 23:48:55 -0700

    new bare

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare: 'https://ohthemisery.d3ch.repl.co/bare/',
+    bare: 'https://165e5daf-3e90-4487-97ce-9fd0869196f3.id.repl.co/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
