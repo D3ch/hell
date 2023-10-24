bfad5c2f2c78a088f7850e2134158f2df457e84c
sss <89742348+sauc23@users.noreply.github.com>
2023-10-24 14:12:24 -0400

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/tiw/',
-    bare: 'https://tomp.app',
+    bare: 'https://dd-neon-chi.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
